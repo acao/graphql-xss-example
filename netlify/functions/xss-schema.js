138380b2d4cf1c65503e899168aa2234137990c4
Rikki Schulte <rikki.schulte@gmail.com>
2021-09-07 17:40:09 +0200

    handle cors

--- netlify/functions/xss-schema.js
+++ netlify/functions/xss-schema.js
@@ -2,20 +2,36 @@
 const schema = require("../../bad-schema");
 
 const isIntrospection = (operationName) => {
-  return operationName?.toLowerCase() === "introspectionquery";
+    return operationName?.toLowerCase() === "introspectionquery";
 };
 
 const mockGraphQLServer = (operationName) => {
-  return isIntrospection(operationName) ? { data: schema } : { data: {} };
+    return isIntrospection(operationName) ? { data: schema } : { data: {} };
 };
 
+const handleOptionsRequest = () => {
+    const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    };
+    return {
+        statusCode: 200,
+        headers,
+        body: JSON.stringify({ message: "Successful preflight request" }),
+    };
+}
+
 module.exports.handler = async function (event) {
+    if (event.httpMethod === "OPTIONS") {
+        return handleOptionsRequest()
+    }
     const operationName = event?.body ? JSON.parse(event.body)?.operationName : null
     return {
-      statusCode: 200,
-      body: JSON.stringify(mockGraphQLServer(operationName)),
-      headers: {
-        'content-type': 'application/json'
-      }
+        statusCode: 200,
+        body: JSON.stringify(mockGraphQLServer(operationName)),
+        headers: {
+            'content-type': 'application/json'
+        }
     }
-  }
+}

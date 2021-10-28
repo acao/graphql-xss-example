f2bdaaeb0f2d803050f29d24072871af2a53c5dd
David Glasser <glasser@apollographql.com>
2021-10-28 12:57:11 -0700

    Allow any headers
    We are "malicious" so we should let the browser do whatever it tries!

--- netlify/functions/xss-schema.js
+++ netlify/functions/xss-schema.js
@@ -5,28 +5,29 @@ const isIntrospection = (operationName) => {
     return operationName?.toLowerCase() === "introspectionquery";
 };
 
-const headers = {
+function headers = (requestHeaders)  => ({
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Headers": requestHeaders['access-control-request-headers'] || "Content-Type",
     "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-};
+    "Vary": "Access-Control-Request-Headers",
+})
 
-const handleOptionsRequest = () => ({
+const handleOptionsRequest = (requestHeaders) => ({
     statusCode: 200,
-    headers,
+    headers(requestHeaders),
     body: JSON.stringify({ message: "Successful preflight request" }),
 })
 
 module.exports.handler = async function (event) {
     if (event.httpMethod === "OPTIONS") {
-        return handleOptionsRequest()
+        return handleOptionsRequest(event.headers)
     }
     const operationName = event?.body ? JSON.parse(event.body)?.operationName : null
     return {
         statusCode: 200,
         body: JSON.stringify({ data: schema }),
         headers: {
-            ...headers,
+            ...(headers(event.headers)),
             'Content-Type': 'application/json'
         }
     }
